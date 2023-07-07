82918ba4ff5bee404db58943a047f1bf05a3ed92
hetashridk <hetashrik@gmail.com>
2023-07-07 23:45:53 +0530

    Trie added

---
+++ Trie/trie.java
@@ -0,0 +1,54 @@
+public class trie {
+
+    public static class Node{
+        Node children[] = new Node[26];
+        boolean eow = false;
+
+        Node(){
+            // to store null in children array
+            for(int i = 0; i < children.length; i++){
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word){
+        Node curr = root;
+
+        for(int level = 0; level < word.length(); level++){
+            int index = word.charAt(level) - 'a';
+            if(curr.children[index] == null){
+                curr.children[index] = new Node();
+            }
+            curr = curr.children[index];
+        }
+
+        curr.eow = true;
+    }
+
+      public static boolean search(String key){
+        Node curr = root;
+
+        for(int level = 0; level < key.length(); level++){
+            int index = key.charAt(level) - 'a';
+            if(curr.children[index] == null){
+                return false;
+            }
+            curr = curr.children[index];
+        }
+
+        return curr.eow == true;
+    }
+    public static void main(String[] args) {
+        String words[] = {"the", "a", "there", "their", "any", "thee"};
+
+        for(int i = 0; i < words.length; i++){
+            insert(words[i]);
+        }
+
+        System.out.println(search("thee"));
+        System.out.println(search("theor"));
+    }
+}
