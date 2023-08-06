24a7b132d8b95e67d130f236ba63d7a2c415609f
hetashridk <hetashrik@gmail.com>
2023-08-06 11:46:24 +0530

    another method of quick sort added

---
+++ Divide & Conquer/quick_sort1.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class quick_sort1 {
+
+    public static void quickSort(int arr[], int low, int high){
+
+        if(low >= high){
+            return;
+        }
+
+        int si = low; 
+        int ei = high;
+        int mid = si + (ei - si)/2;
+
+        int pivot = arr[mid];
+
+        while(si <= ei){
+            while(arr[si] < pivot){
+                si++;
+            }
+            while(arr[ei] > pivot){
+                ei--;
+            }
+
+            // now pivot is at correct position
+            if(si <= ei){
+                // swap
+                int temp = arr[si];
+                arr[si] = arr[ei];
+                arr[ei] = temp;
+                si++;
+                ei--;
+            }
+        }
+
+        quickSort(arr, low, ei);
+        quickSort(arr, si, high);
+    }
+
+
+    public static void main(String[] args) {
+        int arr[] = {2, -1, 4, 10, 1, 0, 40};
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+}
