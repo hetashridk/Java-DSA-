1cb24a4334160ec6c0f89c4907b9d06211614f77
hetashridk <hetashrik@gmail.com>
2023-09-13 23:41:26 +0530

    Fibonnaci using Tabulation solved

--- DP/FibonnaciTabulation.java
+++ DP/FibonnaciTabulation.java
@@ -6,16 +6,17 @@ public class FibonnaciTabulation{
         f[0] = 0;
         f[1] = 1;
 
-        for(int i = 0; i <= n; i++){
+        for(int i = 2; i <= n; i++){
             f[i] = f[i - 1] + f[i - 2];
-            
         }
+
+        return f[n];
 
         
     }
     public static void main(String[] args) {
         int n = 6;
-        int f[] = new int[n + 1];
-        System.out.println(fib(n, f));
+        // int f[] = new int[n + 1];
+        System.out.println(fib(n));
     }
 }
